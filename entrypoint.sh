554e047173653048dcd8336286462b330ef0eb6e
bernd2022 <bernd.nospam.2018@t-online.de>
2025-09-01 07:18:43 +0200

    Container entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+myLogFile=/mnt/hummingbot/logs/entrypoint.log
+
+echo "$(date): ------------------------" >> $myLogFile
+echo "$(date): Entrypoint of Hummingbot" >> $myLogFile
+
+echo "$(date): Create symbolic links" >> $myLogFile
+ln -sf /mnt/hummingbot/certs /home/hummingbot
+ln -sf /mnt/hummingbot/conf /home/hummingbot
+ln -sf /mnt/hummingbot/data /home/hummingbot
+ln -sf /mnt/hummingbot/logs /home/hummingbot
+
+botDir=`printenv BOT_DIR`
+
+if [[ ! -z $botDir ]]
+then
+  echo "$(date): 'BOT_DIR' is ${botDir}" >> $myLogFile
+
+  for subDir in connectors controllers scripts strategies
+  do
+    sourceDir=/home/hummingbot/bots/$botDir/conf/$subDir
+    targetDir=/mnt/hummingbot/conf
+
+    if [[ -e $sourceDir ]]
+    then
+      mkdir -p $targetDir
+      cp -r $sourceDir $targetDir
+    fi
+  done
+else
+  echo "$(date): No 'BOT_DIR' environment variable set" >> $myLogFile
+fi
+
+# Execute the container's main processes
+exec "$@"
